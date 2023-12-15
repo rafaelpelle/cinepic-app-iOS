20c865b83f023ce340322fdb2cfd90402da0d813
Rafael Pelle <rafael.pelle@mercadolivre.com>
2023-12-14 22:03:04 -0300

    feat: initial commit

---
+++ cinepic-app-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cinepic-app-iOS
+//
+//  Created by Rafael Pelle on 14/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
